8f079d22f56d258eb1ae7f6c5df13344266dc52b
Davlatbek <jalolovdavlatbek@gamil.com>
2023-02-10 17:36:02 +0500

    created cals

---
+++ cals.go
@@ -0,0 +1,14 @@
+package cals
+
+func Add(a, b int) int {
+	return a + b
+  }
+  func Sub(a, b int) int {
+	return a / b
+  }
+  func Mutliply(a, b int) int {
+	return a * b
+  }
+  func Remove(a, b int) int {
+	return a - b
+  }
